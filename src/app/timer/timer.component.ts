0d618337e02f838b012d077a5037dba98e75128b
Alfred Mathew <alfredmathew@outlook.com>
2020-08-24 12:55:51 -0400

    changed menu for timer

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
-  constructor() { }
+  public hours: number[];
+  public mins: number[];
+  constructor() { 
+    this.hours = this.initHours();
+    this.mins = this.initMins();
+  }
+
+  initHours(): number[] {
+    let ret_arr: number[] = []; 
+    for(let i = 1; i<=10; i++) {
+      ret_arr.push(i);
+    }
+    return ret_arr;
+  }
+
+  initMins(): number[] {
+    let ret_arr: number[] = []; 
+    for(let i = 1; i<=60; i++) {
+      ret_arr.push(i);
+    }
+    return ret_arr;
+  }
 
   ngOnInit(): void {
   }
+}
 
+export class Timer {
+  seconds: number;
+  passed: number;
+
+  constructor(hrs:number, mins: number) {
+    this.seconds = hrs*3600 + mins*60;
+    this.passed = 0;
+  }
 }
